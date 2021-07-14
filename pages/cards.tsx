c95f2e976b661986716adb647910e4cd63a71ea5
araya_nikukyu <>
2021-07-15 08:38:42 +0900

    各カードに色の追加 カードのサイズ調整、一覧性の向上

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -2,74 +2,89 @@ export const cards = [
   {
     "20": {
       num: 1,
+      color: '#BF1C38',
     },
   },
   {
     "15": {
       num: 2,
+      color: '#BE583A',
     },
   },
   {
     "10": {
       num: 3,
+      color: '#BD5B3A',
     },
   },
   {
     "5": {
       num: 4,
+      color: '#A2AC98',
     },
   },
   {
     "4": {
       num: 4,
+      color: '#A2AC98',
     },
   },
   {
     "3": {
       num: 4,
+      color: '#A2AC98',
     },
   },
   {
     "2": {
       num: 4,
+      color: '#A2AC98',
     },
   },
   {
     "1": {
       num: 4,
+      color: '#A2AC98',
     },
   },
   {
     "0": {
       num: 3,
+      color: '#45AC9E',
     },
     "0 CLEAR": {
       num: 1,
+      color: '#CDEB3B',
     },
   },
   {
     "-5": {
       num: 2,
+      color: '#51ACDD',
     },
   },
   {
     "-10": {
       num: 1,
+      color: '#51ACDD',
     },
   },
   {
     x2: {
       num: 1,
+      color: '#CEE43B',
     },
   },
   {
     "MAX->0": {
       num: 1,
+      color: '#CEE43B',
     },
   },
   {
     "?": {
       num: 1,
+      color: '#CEE43B',
     },
   },
 ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,18 @@ import { cards } from "./cards";
 
 export default function Home() {
   const rows = cards.map((row, key) => (
-    <div key={key} className="flex w-1/2 min-w-max mt-5">
+    <div key={key} className="flex w-1/2 min-w-max mt-1">
       {Object.entries(row).map((v) =>
         [...Array(v[1].num)].map((_, i) => {
           return (
             <div
               key={`${v[0]}_${i}`}
-              className="flex flex-1 justify-center p-2 bg-white rounded-lg shadow-xs ml-5 bg-gradient-to-r from-green-400 to-blue-500"
+              className="flex flex-1 justify-center p-2 bg-white rounded-lg shadow-xs ml-5"
+              style={{ backgroundColor: v[1].color}}
             >
               <div>
-                <span className="text-lg font-bold">{v[0]}</span>
-                <div className="flex items-center justify-center w-full mb-2">
+                <span className="text-sm font-bold">{v[0]}</span>
+                <div className="flex items-center justify-center w-full">
                   <label
                     htmlFor={`${v[0]}_${i}`}
                     className="flex items-center cursor-pointer"
@@ -23,8 +24,8 @@ export default function Home() {
                         id={`${v[0]}_${i}`}
                         className="sr-only"
                       />
-                      <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
-                      <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                      <div className="block bg-gray-600 w-10 h-5 rounded-full"></div>
+                      <div className="dot absolute left-1 top-1 bg-white w-3 h-3 rounded-full transition"></div>
                     </div>
                   </label>
                 </div>
@@ -39,7 +40,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-blue-50">
-        <h1 className="text-4xl font-bold mt-6">Welcome to Coyote</h1>
+        <h1 className="text-xl font-bold mt-1">Welcome to Coyote</h1>
         {rows}
       </main>
 
